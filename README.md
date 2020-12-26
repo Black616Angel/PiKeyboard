93b998481f47db629e2186947425234548ce1a4b
Benjamin Schwarz <the-black-angel@gmx.de>
2020-12-26 14:15:16 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-PiKeyboard
+# PiKeyboard
+
+## Quickstart
+
+Just clone the whole repository and then start the install.bash and reboot afterwards
+```bash
+git clone https://github.com/Black616Angel/PiKeyboard.git
+sudo ./install.bash
+sudo reboot
+```
+
+## The real MVPs
+
+This project is just the barebone of another one by mtlynch called [key-mime-pi](https://github.com/mtlynch/key-mime-pi).
---
+++ install.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -x # Echo commands to stdout.
+set -e # Exit on first error.
+set -u # Treat undefined environment variables as errors.
+
+if ! grep 'dtoverlay=dwc2' /boot/config; then
+  echo "dtoverlay=dwc2" >> /boot/config.txt
+fi
+
+if ! grep dwc2 /etc/modules; then
+  echo "dwc2" >> /etc/modules
+fi
+
+ENABLE_RPI_HID_PATH=/opt/enable-rpi-hid
+mkdir -p "$(dirname $ENABLE_RPI_HID_PATH)"
+mv enable-rpi-hid "$ENABLE_RPI_HID_PATH"
+chmod +x "$ENABLE_RPI_HID_PATH"
+
+mv usb-gadget.service /lib/systemd/system/usb-gadget.service
+
+systemctl daemon-reload
+systemctl enable usb-gadget.service
